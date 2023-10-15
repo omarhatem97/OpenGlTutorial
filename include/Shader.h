ea8c729fc4cca030741b6cbe7f2e49bcfb986816
Omar Hatem <omar.hatem@avidbeam.com>
2023-10-15 16:39:06 +0300

    small refactor

--- include/Shader.h
+++ include/Shader.h
@@ -3,31 +3,28 @@
 #include <string>
 #include <unordered_map>
 
-struct shaderSource {
-    std::string vertexShader;
-    std::string fragmentShader;
+struct ShaderSource {
+  std::string vertexShader;
+  std::string fragmentShader;
 };
 
+class Shader {
+ public:
+  Shader(const std::string& filepath);
+  ~Shader();
 
-class Shader
-{
-public:
-    Shader(const std::string& filepath);
-    ~Shader();
+  void bind() const;
+  void unbind() const;
 
-    void bind() const;
-    void unbind() const;
+  void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+  unsigned int getUnifromLocation(const std::string& name);
 
-    void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
-    unsigned int getUnifromLocation(const std::string& name);
+ private:
+  unsigned int m_rendererId;
+  std::string m_filePath;
+  std::unordered_map<std::string, int> m_uniformLocationCache;
 
-private:
-    unsigned int m_rendererId;
-    std::string m_filePath;
-    std::unordered_map<std::string, int> m_uniformLocationCache;
-
-    shaderSource parseShader(const std::string& filepath);
-    unsigned int compileShader(unsigned int type, const std::string& source);
-    unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader);
+  ShaderSource parseShader(const std::string& filepath);
+  unsigned int compileShader(unsigned int type, const std::string& source);
+  unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader);
 };
-
